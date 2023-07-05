41f4fcbad698dec24afa0bd04a380b645a8cd193
Futas69 <biridindin9@gmail.com>
2023-07-05 10:48:50 -0300

    botão start funcionando

---
+++ Jogo interprise/Assets/Scenes/scrip/Menu.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class Menu : MonoBehaviour
+{
+    public string nomedaCena;
+    public GameObject menuPanel;
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Ativando painel do menu 
+        menuPanel.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    //função do botão start
+    public void StartGame()
+    {
+        // Carregando Cena Game
+        SceneManager.LoadScene(nomedaCena);
+
+    }
+}
